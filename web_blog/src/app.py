7d258041937c69786755db924529a177e3d5033a
sidharth01g <sidharth01g@gmail.com>
2018-05-20 23:17:54 +0530

    Add Flask rendering of Jinja template

--- web_blog/src/app.py
+++ web_blog/src/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -6,7 +6,7 @@ app = Flask(__name__)
 # API endpoints
 @app.route('/')
 def index():
-    return("Hello world")
+    return render_template('login.html')
 
 
 if __name__ == '__main__':
