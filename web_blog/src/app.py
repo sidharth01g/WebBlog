cf64504786caa9ed84d7e243cf7a2c522eb23a97
sidharth01g <sidharth01g@gmail.com>
2018-05-20 13:00:19 +0530

    Add app module

---
+++ web_blog/src/app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+# API endpoints
+@app.route('/')
+def index():
+    return("Hello world")
+
+
+if __name__ == '__main__':
+    app.run(port=4775)
